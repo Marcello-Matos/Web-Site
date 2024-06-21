a2534678cc798ffad9ee0f7bb8fb2cee93b2d49e
Marcello Matos <marcellomatosxads@gmail.com>
2024-06-21 19:01:52 -0300

    alteracao

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-// Import the functions you need from the SDKs you need
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
+
+// src/firebase-config.js
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyClIZMog5pHLj5mtOSyvWp0R_ikPkumHEs",
   authDomain: "web-site-magestic-x-coded.firebaseapp.com",
@@ -19,3 +45,10 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+export { app };
+
+console.log("Inicializando Firebase");
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+console.log("Firebase inicializado", app, analytics);
