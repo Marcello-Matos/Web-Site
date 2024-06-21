355f820478b0054a96f0ece95b0f8d3e9e6b6ea3
Marcello Matos <marcellomatosxads@gmail.com>
2024-06-21 18:53:08 -0300

    alteracao

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-document.getElementById('testimonialForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita o envio do formulário
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-    // Captura os valores dos campos do formulário
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyClIZMog5pHLj5mtOSyvWp0R_ikPkumHEs",
+  authDomain: "web-site-magestic-x-coded.firebaseapp.com",
+  projectId: "web-site-magestic-x-coded",
+  storageBucket: "web-site-magestic-x-coded.appspot.com",
+  messagingSenderId: "482744177613",
+  appId: "1:482744177613:web:352d4fc95c0f3c0eb2aa84",
+  measurementId: "G-F9KJL8BL3Z"
+};
 
-    // Cria um novo elemento de lista para o depoimento
-    const testimonialItem = document.createElement('li');
-    testimonialItem.innerHTML = `<strong>Nome:</strong> ${name}<br><strong>E-mail:</strong> ${email}<br><strong>Depoimento:</strong> ${message}`;
-
-    // Adiciona o novo elemento de lista ao elemento ul
-    document.getElementById('testimonialList').appendChild(testimonialItem);
-
-    // Limpa os campos do formulário
-    document.getElementById('testimonialForm').reset();
-});
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
